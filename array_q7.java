6f532a7878a1e7e8afa9dd2d8cbb33984ce87621
ArghaDey20 <arghadey.work@gmail.com>
2025-08-25 04:44:37 +0530

    Update array_q7.java

--- array_q7.java
+++ array_q7.java
@@ -1,32 +1,33 @@
-
 class Solution {
     public static void moveZeroes(int[] nums) {
-        int count=0;
-        for(int i =0;i<nums.length; i++){
-            if(nums[i]==0){
+        int count = 0;
+        int n = nums.length; // logical size
+        int i = 0;
+
+        while (i < n) {
+            if (nums[i] == 0) {
                 count++;
-                for(int j=i;j<nums.length-1;j++){
-                    nums[j]=nums[j+1];
+                for (int j = i; j < n - 1; j++) {
+                    nums[j] = nums[j + 1];
                 }
-                i--; //recheck the same index becoz we have shifted and there might be a zero at this index
-               
+                n--; // ✅ array shrinks logically
+            } else {
+                i++;
             }
-            
         }
-        System.out.println("\n"+count);
-        while(count>0){
-            nums[nums.length-count]=0;
+
+        // put zeros at the end
+        while (count > 0) {
+            nums[n++] = 0;
             count--;
         }
-    
     }
 
-    public static void main(String args[]){
-        int arr[]={0,3,0,1,0,2,0};
+    public static void main(String args[]) {
+        int arr[] = {0, 0,3,0,2,0,1,0,3,4,2,0,-1,0};
         moveZeroes(arr);
-        for(int i=0; i<arr.length;i++){
-            System.out.print(arr[i]+ " , ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " , ");
         }
-       
     }
 }
