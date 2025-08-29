1ab6fe744b507a3f4758ac6a8e84f42c57e15cf2
ARGHA DEY <arghapalodhi2015@gmail.com>
2025-08-30 04:43:37 +0530

    SORT 0,1,2 ARRAY

---
+++ array_q15_LC75.java
@@ -0,0 +1,29 @@
+class array_q15_LC75 {
+    public void sortColors(int[] nums) {
+        int zero=0,one=0,two=0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        int i=0;
+            while(zero>0){
+                nums[i++]=0;
+                zero--;
+            }
+             while(one>0){
+                nums[i++]=1;
+                one--;
+            }
+             while(two>0){
+                nums[i++]=2;
+                two--;
+            }
+    }
+}
