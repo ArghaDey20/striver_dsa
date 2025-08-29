8998c6623285983c3ad31a1708dfe0788295a7b1
ARGHA DEY <arghapalodhi2015@gmail.com>
2025-08-30 04:28:32 +0530

    Two Sum problem

---
+++ array_q14_LC1.java
@@ -0,0 +1,20 @@
+class array_q14_LC1{
+    
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int arr[]= new int[2];
+        for(int i=0; i<n; i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                    return arr;
+                }
+            }
+        }
+        arr[0]=-1;
+        arr[1]=-1;
+        return arr;
+    }
+}    
+
